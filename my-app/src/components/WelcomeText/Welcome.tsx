b77d8d8f8026cd386ad0eab4a2bb7667acc01cca
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2024-01-02 02:13:44 +0530

    replacedapostrophiewithcode

--- my-app/src/components/WelcomeText/Welcome.tsx
+++ my-app/src/components/WelcomeText/Welcome.tsx
@@ -5,7 +5,7 @@ const Welcome = () =>{
         <div className={styles.Welcome_grand}>
             <div className={styles.Welcome_Parent}>
                 <div className={styles.Welcome_Head}>Welcome to <p>Konstructly</p>.</div>
-                <div className={styles.Welcome_Subhead}>We believe Production is disconnected from Commercial - yet everything that happens on site has a quantifiable impact on a project's profitability.
+                <div className={styles.Welcome_Subhead}>We believe Production is disconnected from Commercial - yet everything that happens on site has a quantifiable impact on a project&apos;s profitability.
 
 We link Progress, Quality and Payments and all the teams / companies involved in a single platform.</div>
             </div>
